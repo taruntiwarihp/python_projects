6d554010ac0046fa9656d4a9115d4ca22000a80b
Tarun Tiwari <54759568+taruntiwarihp@users.noreply.github.com>
2020-07-28 02:29:20 +0530

    Add files via upload

---
+++ msg_plyer.py
@@ -0,0 +1,9 @@
+import time
+from plyer import notification
+if __name__ =="__main__":
+    notification.notify(
+    title="Tarun You need to sleep right now, it's more than 2AM",
+    message="It's important !!",
+    app_icon = "https://github.com/taruntiwarihp/raw_images/blob/master/msgs.ico",
+    timeout=10
+    )
